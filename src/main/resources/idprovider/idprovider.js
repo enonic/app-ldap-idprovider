68d5882ab65fad4846d3087830ec47642e62aea9
ashklianko <asl@enonic.com>
2016-07-15 16:33:08 +0300

    EXT-241 LDAP ID provider (prototype) -Basic things, 99% copied from standard id provider; Calling  authLib.ldapLogin to authenticate; Params sent to be discussed.

---
+++ src/main/resources/idprovider/idprovider.js
@@ -0,0 +1,77 @@
+var mustacheLib = require('/lib/xp/mustache');
+var portalLib = require('/lib/xp/portal');
+var authLib = require('/lib/xp/auth');
+
+exports.handle401 = function (req) {
+    var body = generateLoginPage();
+
+    return {
+        status: 401,
+        contentType: 'text/html',
+        body: body
+    };
+};
+
+exports.get = function (req) {
+    var redirectUrl = generateRedirectUrl();
+    var body = generateLoginPage(redirectUrl);
+
+    return {
+        status: 200,
+        contentType: 'text/html',
+        body: body
+    };
+};
+
+exports.login = function (req) {
+    var redirectUrl = (req.validTicket && req.params.redirect) || generateRedirectUrl();
+    var body = generateLoginPage(redirectUrl);
+
+    return {
+        status: 200,
+        contentType: 'text/html',
+        body: body
+    };
+};
+
+exports.logout = function (req) {
+    authLib.logout();
+    var redirectUrl = (req.validTicket && req.params.redirect) || generateRedirectUrl();
+
+    return {
+        redirect: redirectUrl
+    };
+};
+
+function generateRedirectUrl() {
+    var site = portalLib.getSite();
+    if (site) {
+        return portalLib.pageUrl({id: site._id});
+    }
+    return '/';
+}
+
+function generateLoginPage(redirectUrl) {
+    var jQueryUrl = portalLib.assetUrl({path: "js/jquery-2.2.0.min.js"});
+    var appLoginJsUrl = portalLib.assetUrl({path: "js/login.js"});
+    var appLoginBackgroundUrl = portalLib.assetUrl({path: "common/images/background-1920.jpg"});
+    var appLoginServiceUrl = portalLib.serviceUrl({service: "login"});
+    var authConfig = authLib.getIdProviderConfig();
+    var ldapAddress = authConfig.ldapAddress;
+
+    var configView = resolve('idprovider-config.txt');
+    var config = mustacheLib.render(configView, {
+        appLoginServiceUrl: appLoginServiceUrl,
+        ldapAddress: ldapAddress,
+        redirectUrl: redirectUrl
+    });
+
+    var view = resolve('idprovider.html');
+    var params = {
+        jQueryUrl: jQueryUrl,
+        appLoginJsUrl: appLoginJsUrl,
+        appLoginBackgroundUrl: appLoginBackgroundUrl,
+        config: config
+    };
+    return mustacheLib.render(view, params);
+}
---
+++ src/main/resources/services/login/login.js
@@ -0,0 +1,50 @@
+var portalLib = require('/lib/xp/portal');
+
+function handlePost(req) {
+    var body = JSON.parse(req.body);
+    var userStoreKey = portalLib.getUserStoreKey();
+    var loginResult = ldapLogin({
+        user: body.user,
+        password: body.password,
+        ldapAddress: body.ldapAddress,
+        userStore: userStoreKey.toLowerCase()
+    });
+    return {
+        body: loginResult,
+        contentType: 'application/json'
+    };
+}
+
+function required(params, name) {
+    var value = params[name];
+    if (value === undefined) {
+        throw "Parameter '" + name + "' is required";
+    }
+
+    return value;
+}
+
+/**
+ * Login a user in LDAP
+ *
+ * @param {object} params JSON parameters.
+ * @param {string} params.user Name of user to log in.
+ * @param {string} [params.userStore] Name of user-store where the user is stored.
+ * @param {string} [params.password] Password for the user.
+ * @returns {object} Information for logged-in user.
+ */
+function ldapLogin(params) {
+    var bean = __.newBean('com.enonic.xp.lib.auth.LdapLoginHandler');
+
+    bean.user = required(params, 'user');
+
+    bean.password = required(params, 'password');
+
+    bean.ldapAddress = required(params, 'ldapAddress');
+
+    bean.userStore = required(params, 'userStore');
+
+    return __.toNativeObject(bean.login());
+};
+
+exports.post = handlePost;
